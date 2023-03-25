50de8dec3ed4e35bcd8db2b156497b84a6cfec42
MunnaGupta17 <mgabd17@gmail.com>
2023-03-25 23:18:04 +0530

    spring project created and all necessary connections details and dependency configured

---
+++ Backend/Rest_Api_Task_Scheduler/src/main/java/com/TaskScheduler/RestApiTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.TaskScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ Backend/Rest_Api_Task_Scheduler/src/test/java/com/TaskScheduler/RestApiTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
